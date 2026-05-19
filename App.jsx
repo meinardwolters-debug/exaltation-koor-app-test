882f274bede3a3218f91d3f1c3bff9be2d618148
meinardwolters-debug <meinardwolters@gmail.com>
2026-05-19 15:00:15 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+{
+  "name": "Exaltation Aanmeld App",
+  "short_name": "Exaltation",
+  "start_url": "/",
+  "scope": "/",
+  "display": "standalone",
+  "background_color": "#020617",
+  "theme_color": "#020617",
+  "icons": [
+    {
+      "src": "/exaltation.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/exaltation.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
